2bb02efd46c2da2294f699b666994460c9124330
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-12-14 17:36:56 -0500

    Updated to mock requests for testing

--- test_teams.py
+++ test_teams.py
@@ -2,16 +2,39 @@ from typing import Generator
 
 import pytest
 
-from teams import Teams, StartEndSeason
+from teams import TeamsAPI, TeamsData, StartEndSeason
 
 class TestTeams:
 
     @pytest.fixture
+    def teams_api(self) -> Generator:
+        yield TeamsAPI()
+
+    @pytest.fixture
     def teams(self) -> Generator: 
-        yield Teams()
+        yield TeamsData(TeamsAPI())
 
     def test_season(self, teams):
         team_seasons = teams.split_season_years(20202021)
 
         assert team_seasons.start == 2020
         assert team_seasons.end == 2021
+
+    def test_pull_teams(self, teams_api, mocker):
+
+        mock_data = {"data":
+                     [
+                         {"id":32,"franchiseId":27,"fullName":"Quebec Nordiques","leagueId":133,"rawTricode":"QUE","triCode":"QUE"},
+                         {"id":8,"franchiseId":1,"fullName":"Montréal Canadiens","leagueId":133,"rawTricode":"MTL","triCode":"MTL"},
+                         {"id":58,"franchiseId":5,"fullName":"Toronto St. Patricks","leagueId":133,"rawTricode":"TSP","triCode":"TSP"},
+                         {"id":7,"franchiseId":19,"fullName":"Buffalo Sabres","leagueId":133,"rawTricode":"BUF","triCode":"BUF"},],
+        }
+
+        mock_response = mocker.MagicMock()
+        mock_response.json.return_value = mock_data
+
+        mocker.patch('requests.get', return_value=mock_response)
+
+        result = teams_api.pull_teams()
+
+        assert len(result['data']) == 4
