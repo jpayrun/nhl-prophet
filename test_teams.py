c8f85d303f6596333c12404a1e814df69b6bb7e1
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-12-14 17:45:02 -0500

    Added test to ensure valid data working

--- test_teams.py
+++ test_teams.py
@@ -38,3 +38,21 @@ class TestTeams:
         result = teams_api.pull_teams()
 
         assert len(result['data']) == 4
+
+    def test_pull_teams_missing_data_key(self, teams_api, mocker):
+
+        mock_data = {"missing key":
+                     [
+                         {"id":32,"franchiseId":27,"fullName":"Quebec Nordiques","leagueId":133,"rawTricode":"QUE","triCode":"QUE"},
+                         {"id":8,"franchiseId":1,"fullName":"Montréal Canadiens","leagueId":133,"rawTricode":"MTL","triCode":"MTL"},
+                         {"id":58,"franchiseId":5,"fullName":"Toronto St. Patricks","leagueId":133,"rawTricode":"TSP","triCode":"TSP"},
+                         {"id":7,"franchiseId":19,"fullName":"Buffalo Sabres","leagueId":133,"rawTricode":"BUF","triCode":"BUF"},],
+        }
+
+        mock_response = mocker.MagicMock()
+        mock_response.json.return_value = mock_data
+
+        mocker.patch('requests.get', return_value=mock_response)
+
+        with pytest.raises(ValueError) as test:
+            result = teams_api.pull_teams()
