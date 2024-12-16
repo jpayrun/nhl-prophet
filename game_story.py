c805ae7a9131e715952a498f101ce494e9c54d59
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-12-15 21:33:56 -0500

    Adding writing raw file

--- game_story.py
+++ game_story.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import json
 import logging
+from pathlib import Path
 from typing import Any
 
 import requests
@@ -48,6 +49,67 @@ class GameStoryAPI(IGameStoryAPI):
         except requests.exceptions.RequestException:
             logging.error("Error pulling data for {url}")
             raise RuntimeError("Error pulling data for")
+
+
+class GameStoryData:
+
+    def __inti__(self, game_story_api: IGameStoryAPI) -> None:
+        """
+        Constructor
+
+        Args:
+            game_story_api (IGameStoryAPI): Interface for game story api data
+        """
+        self.game_story_api = game_story_api
+
+    def pull_data(self, game_id: int) -> Any:
+        return self.game_story_api.pull_data
+
+
+class IWriteGameStory(ABC):
+
+    @abstractmethod
+    def raw_data(self) -> None:
+        raise NotImplementedError
+
+
+class WriteGameStoryLocal(IWriteGameStory):
+
+    def __init__(self, game_data: GameStoryData) -> None:
+        """
+        Constructor
+
+        Args:
+            game_data (GameStoryData): Game Data Instance
+        """
+        self.game_data = game_data
+
+    def raw_data(self,
+                 game_id: int,
+                 path: str = './raw/',
+                 file_name: Path = 'game_story') -> None:
+        """
+        Write the raw game data to a file
+
+        Args:t
+            game_id (int): Game id
+            path (str, optional): Path to store raw data. Defaults to './raw/'.
+            file_name (Path, optional): File Name. Defaults to 'game_story'.
+
+        Raises:
+            RuntimeError: Error running the script
+        """
+        data = self.game_data.pull_data(game_id=game_id)
+        data = json.dumps(data)
+        file = Path(path + file_name + '_' str(game_id) + '.json')
+        try:
+            logging.info(f"Writing file for game {game_id}")
+            file.parent.mkdir(parents=True, exist_ok=True)
+            file.write_text(data, encoding='utf-8')
+            logging.info(f"Wrote file for game {game_id}")
+        except Exception as e:
+            logging.error(f"Error writing file for game {game_id} error {e}")
+            raise RuntimeError(f"Error writing file {e}")
 
 
 if __name__ == '__main__':
