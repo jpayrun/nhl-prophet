ad28402ce6d06fb4fb28888f42c168280e72394c
Jordan <27322284+jpayrun@users.noreply.github.com>
2024-12-15 21:44:28 -0500

    Updated the error codes

--- game_story.py
+++ game_story.py
@@ -41,19 +41,19 @@ class GameStoryAPI(IGameStoryAPI):
     def pull_data(self, game: int) -> Any:
         url = self.base_url + self.end_point + str(game)
         try:
-            logging.info("Pulling data from {url}")
+            logging.info(f"Pulling data from {url}")
             r = requests.get(url)
             r.raise_for_status()
             data = r.json()
             return data
-        except requests.exceptions.RequestException:
-            logging.error("Error pulling data for {url}")
-            raise RuntimeError("Error pulling data for")
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error pulling data for {url}")
+            raise RuntimeError(f"Error pulling data for {url} error {e}")
 
 
 class GameStoryData:
 
-    def __inti__(self, game_story_api: IGameStoryAPI) -> None:
+    def __init__(self, game_story_api: IGameStoryAPI) -> None:
         """
         Constructor
 
@@ -63,7 +63,16 @@ class GameStoryData:
         self.game_story_api = game_story_api
 
     def pull_data(self, game_id: int) -> Any:
-        return self.game_story_api.pull_data
+        """
+        Pull data for the game
+
+        Args:
+            game_id (int): Game to pull data for
+
+        Returns:
+            Any: Json of the game data
+        """
+        return self.game_story_api.pull_data(game)
 
 
 class IWriteGameStory(ABC):
@@ -99,11 +108,11 @@ class WriteGameStoryLocal(IWriteGameStory):
         Raises:
             RuntimeError: Error running the script
         """
-        data = self.game_data.pull_data(game_id=game_id)
-        data = json.dumps(data)
-        file = Path(path + file_name + '_' str(game_id) + '.json')
         try:
             logging.info(f"Writing file for game {game_id}")
+            data = self.game_data.pull_data(game_id=game_id)
+            data = json.dumps(data)
+            file = Path(path + file_name + '_' + str(game_id) + '.json')
             file.parent.mkdir(parents=True, exist_ok=True)
             file.write_text(data, encoding='utf-8')
             logging.info(f"Wrote file for game {game_id}")
@@ -117,4 +126,5 @@ if __name__ == '__main__':
     game = 2024020586
     api = GameStoryAPI()
     data = GameStoryData(api)
+    # print(data.pull_data(game_id=game))
     WriteGameStoryLocal(data).raw_data(game_id=game)
